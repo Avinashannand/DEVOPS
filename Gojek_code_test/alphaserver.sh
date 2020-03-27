ba11cc12442ae68ddbb47b0ee25541629c95c4e5
Avinash Kumar <Avinashannand@users.noreply.github.com>
2020-03-27 15:43:29 +0530

    Update alphaserver.sh

--- Gojek_code_test/alphaserver.sh
+++ Gojek_code_test/alphaserver.sh
@@ -2,29 +2,21 @@
 set -x
 
 #if we have to another server where we need to reflects the client's server metrics details.
+#Server_ip=X.X.X.X
+#Username=admin
 
-Server_ip=X.X.X.X
-Username=admin
-
-function alphaserver() {
+#function alphaserver() {
 
 #Prerequists that ssh from alphaserver to alphaclient is password less.
-scp -r {Path_of_directory}/alphaclient.sh $Username@$Server_ip:/home/{$Username}
-
-
-ssh $Username@$Server_ip << EOF
-
-chmod 777 /home/{$Username}/alphaclient.sh
-
-
+#scp -r {Path_of_directory}/alphaclient.sh $Username@$Server_ip:/home/{$Username}
+#ssh $Username@$Server_ip << EOF
+#chmod 777 /home/{$Username}/alphaclient.sh
 # It is assumed that client_node mentioned in the alphaclient.sh file should easily authenticated from this alphaserver machine.
+#./home/{$Username}/alphaclient.sh
 
-./home/{$Username}/alphaclient.sh
-
-EOF
-}
-
-alphaserver
+#EOF
+#}
+#alphaserver
 
 
 #if we are on same server where we have to display the metrics of clients server details.
